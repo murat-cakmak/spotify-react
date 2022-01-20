831feeb1a43c940bc70b6bbb143c93b56134a868
Murat Cakmak <mrtstab@gmail.com>
2022-01-21 01:54:25 +0300

    spotfy clone update 2

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Sidebar from "components/Sidebar";
+import Content from "components/Content";
+import BottomBar from "components/BottomBar";
+import {
+    BrowserRouter as Router,
+} from "react-router-dom";
+
+
+function App() {
+    return (
+        <Router>
+            <div className={"wrapper"}>
+                <Sidebar></Sidebar>
+                <Content></Content>
+            </div>
+            <BottomBar />
+        </Router>
+    )
+}
+
+export default App;
